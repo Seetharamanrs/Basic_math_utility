6a2c3d7d4d9f93b3c5b68b469db0842781c95210
seetharamanrs <seetharaman2632001@gmail.com>
2025-06-12 20:09:05 +0100

    Chenges according to the app.py

--- unitconverter.py
+++ unitconverter.py
@@ -5,10 +5,10 @@ class unitconverter:
         b=b.lower()
         if b=="fahrenheit":
             f=(a-32)*(5/9)
-            return round(f,2)
+            return f" {f:.2f} Celsius"
         elif b=="celsius":
             c=(a*(9/5))+32
-            return round(c,2)
+            return f"{c:.2f} Fahrenheit"
         else:
             return "INVALID!!"
     def weight(self,a,b):
